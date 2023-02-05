2c2035a6c219f3ed945d5b9f5caef479c7392489
Manish Madan <96079232+ManishMadan2882@users.noreply.github.com>
2023-02-05 20:00:49 +0530

    correction

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import '../index.css'
 
 function Card(props) {
   return (
-    <div className='m-6 drop-shadow-2xl text-slate-800 hover:text-gray-300 h-[130px] w-bg-gradient-to-r from-green-400 to-blue-400 max p-6 cursor-pointer rounded-2xl hover:from-emerald-500 hover:to-indigo-500 ...'>
+    <div className='m-6 drop-shadow-2xl text-slate-800 hover:text-gray-300 h-[130px] w-max p-6 cursor-pointer rounded-2xl bg-gradient-to-r from-green-400 to-blue-400 hover:from-emerald-500 hover:to-indigo-500 ...'>
        <h1 className='text-3xl font-extrabold'>{props.title}</h1>
        <h1 className='text-2xl'>{props.value}</h1>
     </div>
